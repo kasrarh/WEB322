120db542d5a2be69f7bb48ec5ece5802829bc12d
kasrarh <55912648+kasrarh@users.noreply.github.com>
2022-10-19 22:58:36 -0400

    Login validation implemented

--- server.js
+++ server.js
@@ -17,29 +17,29 @@ app.use("/pictures", exp.static(path.join(__dirname, "pictures")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/blog.hbs"));
+    res.render("blog", { layout: false });
 });
 app.get("/Blog", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/blog.hbs"));
+    res.render("blog", { layout: false });
 });
 app.get("/article/:id", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/article.hbs"));
+    res.render("article", { layout: false });
 });
 app.get("/login", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/login.hbs"));
+    res.render("login", { layout: false });
 });
 app.get("/register", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/register.hbs"));
+    res.render("register", { layout: false });
 });
 
 app.post("/login", function (req, res) {
     let body = req.body;
+    console.log(body.username)
     if (body.username && body.password && specialChars(body.username) == false) {
       res.render("blog", { layout: false });
     } else {
-      var loginErrorMsg =
-        "the username or the password is inccorect(username can not conatin special charcters)";
-      res.render("login", { loginErrorMsg: loginErrorMsg, layout: false });
+      var loginErrorMsg = "The username contains special characters";
+      res.render("login", { layout: false, loginErrorMsg: loginErrorMsg , username: String(body.username)});
     }
   });
 
