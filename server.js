96751e87691869ad7280c35a7be36a785793c872
kasrarh <55912648+kasrarh@users.noreply.github.com>
2022-10-19 20:45:30 -0400

    modifying server.js creating hbs files

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+var port = process.env.PORT || 3000;
 
-var HTTP_PORT = process.env.PORT || 3000;
+const exp = require("express");
+const app = exp();
+const path = require("path");
 
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname,"login"));
+app.use("/pictures", exp.static(path.join(__dirname, "pictures")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "blog.hbs"));
+});
+app.get("/Blog", function (req, res) {
+  res.sendFile(path.join(__dirname, "blog.hbs"));
+});
+app.get("/article/:id", function (req, res) {
+  res.sendFile(path.join(__dirname, "article.hbs"));
+});
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "login.hbs"));
+});
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "register.hbs"));
 });
 
-app.listen(HTTP_PORT, onHttpStart);
+app.listen(port);
